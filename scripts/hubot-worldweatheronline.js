83af2d6d3de39d51933a5f96f56b5f9c1943aa1d
Yuriy Opryshko <mcm6987@gmail.com>
2015-04-28 18:14:23 +0300

    initial commit

---
+++ scripts/hubot-worldweatheronline.js
@@ -0,0 +1,23 @@
+/*
+# Description:
+#   Provides current weather conditions and forecast, courtesy of worldweatheronline.com
+#
+# Dependencies:
+#   "moment": "2.10.x"
+#
+# Configuration:
+#   WWE_API_KEY - your worldweatheronline.com API key
+#
+# Commands:
+#   hubot weather <location> - current weather conditions and forecast for tomorrow for <location>
+#   hubot forecast <location> - seven-day weather forecast for <location>
+#
+# Author:
+#   mcm69
+*/
+
+var moment = require('moment');
+
+module.exports = function(robot) {
+
+};
