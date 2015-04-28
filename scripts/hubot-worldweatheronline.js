7b3e2c6ece1b53dcb79a22b61121a30f53919782
Yuriy Opryshko <mcm6987@gmail.com>
2015-04-29 00:05:03 +0300

    display more data for the weather command

--- scripts/hubot-worldweatheronline.js
+++ scripts/hubot-worldweatheronline.js
@@ -1,24 +1,30 @@
-/*
-# Description:
-#   Provides current weather conditions and forecast, courtesy of worldweatheronline.com
-#
-# Dependencies:
-#   "moment": "2.10.x"
-#
-# Configuration:
-#   WWE_API_KEY - your worldweatheronline.com API key
-#
-# Commands:
-#   hubot weather <location> - current weather conditions and detailed 2-day forecast for <location>
-#   hubot forecast <location> - seven-day weather forecast for <location>
-#
-# Author:
-#   mcm69
-*/
 
+//Description:
+//  Provides current weather conditions and forecast, courtesy of worldweatheronline.com
+//
+//Dependencies:
+//  "moment": "2.10.x"
+//
+//Configuration:
+//  WWE_API_KEY - your worldweatheronline.com API key
+//
+//Commands:
+//  hubot weather <location> - current weather conditions and detailed 2-day forecast for <location>
+//  hubot forecast <location> - seven-day weather forecast for <location>
+//
+//Author:
+//  mcm69
 var WEATHER_TEXT = 'Current Weather in %s: %s, %d°C (feels like %d°C)\n' +
                    'Wind %s, %dkm/h. Observed at %s';
-
+var DAILY_TEXT = 'Forecast for %s:\n' +
+                 'High %d°C, low %d°C. UV Index %d.' +
+                 'Sunrise at %s, sunset at %s.\n' +
+                 'Hourly forecast:\n%s';
+var HOURLY_TEXT = '%s: %d°C (feels like %d°C), wind %s, %dkm/h. %s';
+var WEATHER_URL = 'http://api.worldweatheronline.com/free/v2/weather.ashx?q=%s' +
+    '&format=json&num_of_days=2&fx24=yes&showlocaltime=yes&key=%s';
+var FORECAST_URL = 'http://api.worldweatheronline.com/free/v2/weather.ashx?q=%s' +
+    '&format=json&num_of_days=7&showlocaltime=yes&key=%s';
 
 var API_KEY = process.env.WWE_API_KEY;
 var util = require('util'),
@@ -27,6 +33,7 @@ var util = require('util'),
 util.log("Starting hubot-worldweatheronline.");
 if (!API_KEY) {
     util.log("No API key set!");
+    module.exports = function(){};
     return;
 }
 
@@ -37,9 +44,7 @@ module.exports = function(robot) {
             return;
         }
         util.log('Got request for weather in ' + location);
-        var url = 'http://api.worldweatheronline.com/free/v2/weather.ashx?q='+
-            escape(location) +
-            '&format=json&fx24=yes&showlocaltime=yes&key=' + API_KEY;
+        var url = format(WEATHER_URL, escape(location, API_KEY));
         robot.http(url).get()(function(err, res, body) {
             if (err) {
                 msg.send('Can\'t get weather for ' + location + ', an error occurred.');
@@ -67,6 +72,31 @@ module.exports = function(robot) {
                 currentCondition.windspeedKmph,
                 currentCondition.observation_time
             );
+
+            for (var i = 0; i < data.weather.length; i++) {
+                var dayWeather = data.weather[i];
+                var dayText = format(DAILY_TEXT,
+                    dayWeather.date,
+                    dayWeather.maxtempC,
+                    dayWeather.mintempC,
+                    dayWeather.uvIndex,
+                    dayWeather.astronomy[0].sunrise,
+                    dayWeather.astronomy[0].sunset,
+                    dayWeather.hourly
+                        .map(function(hrly) {
+                            return format(HOURLY_TEXT,
+                                hrly.time,
+                                hrly.tempC,
+                                hrly.FeelsLikeC,
+                                hrly.winddir16Point,
+                                hrly.windspeedKmph,
+                                hrly.weatherDesc[0].value
+                            );
+                        }).join('\n')
+                );
+
+                response += '\n\n' + dayText;
+            }
 
             msg.send(response);
         });
