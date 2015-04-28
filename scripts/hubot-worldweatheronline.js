140639b142c0af6e6f11bb4fb656e5ae86a8643d
Yuriy Opryshko <mcm6987@gmail.com>
2015-04-28 19:48:18 +0300

    basic implementation of weather command

--- scripts/hubot-worldweatheronline.js
+++ scripts/hubot-worldweatheronline.js
@@ -9,15 +9,58 @@
 #   WWE_API_KEY - your worldweatheronline.com API key
 #
 # Commands:
-#   hubot weather <location> - current weather conditions and forecast for tomorrow for <location>
+#   hubot weather <location> - current weather conditions and detailed 2-day forecast for <location>
 #   hubot forecast <location> - seven-day weather forecast for <location>
 #
 # Author:
 #   mcm69
 */
 
-var moment = require('moment');
+var WEATHER_TEXT = 'Current Weather in %s: %s, %d°C (feels like %d°C)\n' +
+                   'Wind %s, %dkm/h. Observed at %s';
+
+
+var API_KEY = process.env.WWE_API_KEY;
+var format = require('util').format;
 
 module.exports = function(robot) {
+    robot.respond(/weather (.*)/i, function(msg) {
+        var location = msg.match[1];
+        if (!location) {
+            return;
+        }
+        var url = 'http://api.worldweatheronline.com/free/v2/weather.ashx?q='+
+            escape(location) +
+            '&format=json&fx24=yes&showlocaltime=yes&key=' + API_KEY;
+        robot.http(url).get()(function(err, res, body) {
+            if (err) {
+                msg.send('Can\'t get weather for ' + location + ', an error occurred.');
+                return;
+            }
+            if (res.statusCode !== 200) {
+                msg.send('Can\'t get weather for ' + location + ', got HTTP code ' + res.statusCode);
+                return;
+            }
+            var data = JSON.parse(body).data;
 
+            if (data.error) {
+                msg.send('Can\'t get weather for ' + location + ', the server said: ' + data.error[0].msg);
+                return;
+            }
+
+            var currentCondition = data.current_condition[0];
+
+            var response = format(WEATHER_TEXT,
+                data.request[0].query,
+                currentCondition.weatherDesc[0].value,
+                currentCondition.temp_C,
+                currentCondition.FeelsLikeC,
+                currentCondition.winddir16Point,
+                currentCondition.windspeedKmph,
+                currentCondition.observation_time
+            );
+
+            msg.send(response);
+        });
+    });
 };
